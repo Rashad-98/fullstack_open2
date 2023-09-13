a616d93c2b26e3da5cfe97bcb3e3b4d46002d953
Abdelrahman Rashad <a.rashaad98@gmail.com>
2023-09-13 19:09:11 +0300

    3.4: Phonebook backend step4

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const express = require('express')
 const app = express()
 
-const persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
 ]
 
 app.get('/api/persons', (request, respons) => {
@@ -29,9 +29,17 @@ app.get('/api/persons', (request, respons) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const person = persons.find(person => person.id === Number(request.params.id))
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
   if (!person) response.status(404).end()
   response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
 })
 
 app.get('/info', (request, response) => {
